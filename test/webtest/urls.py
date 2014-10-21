54ac00079c7fd27a79852d0adde54ae91573b5eb
Alberto Alcolea <albertoalcolea@gmail.com>
2014-10-22 00:40:17 +0200

    Added bootstrap static template

--- test/webtest/urls.py
+++ test/webtest/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 from django.contrib import admin
 
 urlpatterns = patterns('',
@@ -9,3 +12,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('main.urls', namespace='main')),
 )
+
+# Static content
+urlpatterns += staticfiles_urlpatterns()
